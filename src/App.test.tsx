fe1b265a304c3f2d2ef3498d6de2cfc18ad34d84
MahmoudAhmed294 <mahmoudyoussef.dev@gmail.com>
2026-08-11 20:10:34 +0300

    fix: the app failds test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,10 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import App from './App'
 
 describe('App', () => {
   it('renders the Get started heading', () => {
     render(<App />)
-    expect(screen.getByRole('heading', { name: 'Get started' })).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: 'Welcome to Ci CD pipeline with React and Vite' })).toBeInTheDocument()
   })
 
-  it('increments the counter when clicked', async () => {
-    const user = userEvent.setup()
-    render(<App />)
-
-    const button = screen.getByRole('button', { name: /count is/i })
-    expect(button).toHaveTextContent('Count')
-
-    await user.click(button)
-    expect(button).toHaveTextContent('Count is 1')
-  })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center"></section>
+      <h1 id="center">
+        Welcome to Ci CD pipeline with React and Vite
+      </h1>
     </>
   )
 }
